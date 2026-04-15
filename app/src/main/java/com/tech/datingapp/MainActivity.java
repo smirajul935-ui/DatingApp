1bb16d6b5fe0c823df673df27ff03cd51720b65e
smirajul935-ui <smirajul935@gmail.com>
2026-04-15 11:39:17 +0530

    Refactor button click listener for server connection

--- app/src/main/java/com/tech/datingapp/MainActivity.java
+++ app/src/main/java/com/tech/datingapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.tech.datingapp;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -17,24 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); 
 
-        TextView tvStatus = findViewById(R.id.tvStatus);
-        Button btnConnect = findViewById(R.id.btnConnect);
+        // Variables
+        final TextView tvStatus = findViewById(R.id.tvStatus);
+        final Button btnConnect = findViewById(R.id.btnConnect);
 
-        btnConnect.setOnClickListener(v -> {
-            tvStatus.setText("Connecting...");
-            btnConnect.setEnabled(false); 
-            
-            String finalUrl = SERVER_URL + "?channelName=testRoom&uid=123";
-            StringRequest request = new StringRequest(Request.Method.GET, finalUrl,
-                response -> {
-                    tvStatus.setText("Token:\n" + response);
-                    btnConnect.setEnabled(true);
-                },
-                error -> {
-                    tvStatus.setText("Failed!");
-                    btnConnect.setEnabled(true);
-                });
-            Volley.newRequestQueue(this).add(request);
+        // Button Click Event (Bina shortcut ke)
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvStatus.setText("Connecting to Server...");
+                btnConnect.setEnabled(false); 
+                
+                String finalUrl = SERVER_URL + "?channelName=testRoom&uid=123";
+                
+                StringRequest request = new StringRequest(Request.Method.GET, finalUrl,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            tvStatus.setText("Token Received:\n" + response);
+                            btnConnect.setEnabled(true);
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            tvStatus.setText("Connection Failed!");
+                            btnConnect.setEnabled(true);
+                        }
+                    });
+                    
+                Volley.newRequestQueue(MainActivity.this).add(request);
+            }
         });
     }
 }
